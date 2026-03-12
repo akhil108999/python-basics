c326f475311e13db68700339696408d7aa75a5e7
akhilrjs1089 <93479685+akhil108999@users.noreply.github.com>
2026-03-12 09:42:43 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,36 @@
+# name = "bro code!"
+# if name[0].islower():
+#    name = name.capitalize()
+#    print(name)
+
+# first_name = name[:3].upper()
+# last_name = name[4:].lower()
+# last_characters = name[-1]
+# print(first_name)
+# print(last_name)
+# print(last_characters)
+
+
+# numbers = [10, 20, 30]
+# numbers[1] = 50
+# print(numbers)
+
+
+# Print the first element using indexing.
+# numbers = [10, 20, 30, 40]
+# print(numbers[0])
+
+
+# Print the last element using negative indexing.
+# numbers = [5, 15, 25, 35]
+
+# print(numbers[-1])
+
+
+# Print the third character.
+
+word = "Python"
+print(word[2])
+
+student = {"name": "Ram", "age": 21}
+print(student["name"])
